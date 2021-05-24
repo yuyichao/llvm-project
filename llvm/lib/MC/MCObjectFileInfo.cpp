3d0235ab31e33965a4e2e5fadcba8ec8e87e2391
Cody Tapscott <cody+github@tapscott.me>
2021-05-24 16:36:06 -0700

    Force `.eh_frame` emission on AArch64
    We need to force the emission of the EH Frame section (currently done via SupportsCompactUnwindWithoutEHFrame in the MCObjectFileInfo for the target), since libunwind doesn't yet support dynamically registering compact unwind information at run-time.
    
    (cherry picked from commit 60e041894288848e37870c42749a1aabcc2c2274)
    (cherry picked from commit 6275013da5e8cd5e552bd5bb7d85c7b0524ca69d)
    (cherry picked from commit b62bce122191cef0079a1bea7d990c1225de488f)
    (cherry picked from commit e2adfc20509b970c1726ae15230e982d4be2b918)
    (cherry picked from commit 702f88a8d281645305547da38189e90606b9e61d)

--- llvm/lib/MC/MCObjectFileInfo.cpp
+++ llvm/lib/MC/MCObjectFileInfo.cpp
@@ -69,10 +69,11 @@ void MCObjectFileInfo::initMachOMCObjectFileInfo(const Triple &T) {
           MachO::S_ATTR_STRIP_STATIC_SYMS | MachO::S_ATTR_LIVE_SUPPORT,
       SectionKind::getReadOnly());
 
-  if (T.isOSDarwin() &&
-      (T.getArch() == Triple::aarch64 || T.getArch() == Triple::aarch64_32 ||
-      T.isSimulatorEnvironment()))
-    SupportsCompactUnwindWithoutEHFrame = true;
+  // Disabled for now, since we need to emit EH Frames for stack unwinding in the JIT
+  //if (T.isOSDarwin() &&
+  //    (T.getArch() == Triple::aarch64 || T.getArch() == Triple::aarch64_32 ||
+  //    T.isSimulatorEnvironment()))
+  //  SupportsCompactUnwindWithoutEHFrame = true;
 
   switch (Ctx->emitDwarfUnwindInfo()) {
   case EmitDwarfUnwindType::Always:
