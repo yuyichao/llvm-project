d45bcc00529ec9fc74db49e88c934e255afee17a
David Nadlinger <code@klickverbot.at>
2021-12-30 04:23:23 +0100

    [JITLink] Also include EHFrame* passes on MachO_arm64
    Without the non-compact unwind info there is no way to register
    the EH frames with libunwind (e.g. for backtrace generation).

--- llvm/lib/ExecutionEngine/JITLink/MachO_arm64.cpp
+++ llvm/lib/ExecutionEngine/JITLink/MachO_arm64.cpp
@@ -683,6 +683,10 @@ void link_MachO_arm64(std::unique_ptr<LinkGraph> G,
     else
       Config.PrePrunePasses.push_back(markAllSymbolsLive);
 
+    Config.PrePrunePasses.push_back(EHFrameSplitter("__TEXT,__eh_frame"));
+    Config.PrePrunePasses.push_back(EHFrameEdgeFixer("__TEXT,__eh_frame",
+        8, Delta64, Delta32, NegDelta32));
+
     // Add an in-place GOT/Stubs pass.
     Config.PostPrunePasses.push_back(
         PerGraphGOTAndPLTStubsBuilder_MachO_arm64::asPass);
