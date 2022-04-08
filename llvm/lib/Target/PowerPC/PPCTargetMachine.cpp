10abac61ff684d2b02d23798cb77f65264106ae0
Chen Zheng <czhengsz@cn.ibm.com>
2022-04-08 03:16:05 -0400

    [PowerPC] add a new pass to expand ctr loop pseudos
    This patch implements a new way to generate the CTR loops. Now the
    intrinsics inserted in hardware loop pass will be mapped to pseudo
    instructions and these pseudo instructions will be expanded to CTR
    loop or normal compare+branch loop in this post ISEL pass.
    
    Reviewed By: lkail
    
    Differential Revision: https://reviews.llvm.org/D122125
    
    (cherry picked from commit a71fe49bb534fda18edf57284e16d1705fc54879)

--- llvm/lib/Target/PowerPC/PPCTargetMachine.cpp
+++ llvm/lib/Target/PowerPC/PPCTargetMachine.cpp
@@ -125,6 +125,7 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializePowerPCTarget() {
   initializePPCLowerMASSVEntriesPass(PR);
   initializePPCExpandAtomicPseudoPass(PR);
   initializeGlobalISel(PR);
+  initializePPCCTRLoopsPass(PR);
 }
 
 static bool isLittleEndianTriple(const Triple &T) {
@@ -521,6 +522,16 @@ void PPCPassConfig::addPreRegAlloc() {
   }
   if (EnableExtraTOCRegDeps)
     addPass(createPPCTOCRegDepsPass());
+
+  // Run CTR loops pass before MachinePipeliner pass.
+  // MachinePipeliner will pipeline all instructions before the terminator, but
+  // we don't want DecreaseCTRPseudo to be pipelined.
+  // Note we may lose some MachinePipeliner opportunities if we run CTR loops
+  // generation pass before MachinePipeliner and the loop is converted back to
+  // a normal loop. We can revisit this later for running PPCCTRLoops after
+  // MachinePipeliner and handling DecreaseCTRPseudo in MachinePipeliner pass.
+  if (getOptLevel() != CodeGenOpt::None)
+    addPass(createPPCCTRLoopsPass());
 
   if (getOptLevel() != CodeGenOpt::None)
     addPass(&MachinePipelinerID);
