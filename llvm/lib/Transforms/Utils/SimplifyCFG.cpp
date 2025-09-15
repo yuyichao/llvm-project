422179dd6ee8d6b84023f922f3a0864db6e07c68
William Moses <gh@wsmoses.com>
2025-09-14 22:50:42 -0500

    [SimplifyCFG] Refine metadata handling during instruction hoisting (#158448)
    Co-authored-by: Nikita Popov <npopov@redhat.com>
    (cherry picked from commit c44e015b49e832c9f3749c33cf5c9d5aacaf60a4)

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -3034,7 +3034,7 @@ bool SimplifyCFGOpt::SpeculativelyExecuteBB(BranchInst *BI, BasicBlock *ThenBB,
     if (!SpeculatedStoreValue || &I != SpeculatedStore) {
       // Don't update the DILocation of dbg.assign intrinsics.
       if (!isa<DbgAssignIntrinsic>(&I))
-        I.setDebugLoc(DebugLoc());
+        I.dropLocation();
     }
     I.dropUndefImplyingAttrsAndUnknownMetadata();
 
