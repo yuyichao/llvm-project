0cb00119ed305c9716e9c9e2e5ea2a8e44e106e4
Yichao Yu <yyc1992@gmail.com>
2025-12-28 17:41:01 -0500

    Slightly simplify and optimize symmetric operation opcode check
    We are already confirming that everything is consistent with the first operation so there's no need to check the opcode for every single instructions

--- llvm/lib/CodeGen/ComplexDeinterleavingPass.cpp
+++ llvm/lib/CodeGen/ComplexDeinterleavingPass.cpp
@@ -973,8 +973,8 @@ static bool isInstructionPairAdd(Instruction *A, Instruction *B) {
          (OpcA == Instruction::Add && OpcB == Instruction::Sub);
 }
 
-static bool isInstructionPotentiallySymmetric(Instruction *I) {
-  switch (I->getOpcode()) {
+static bool isInstructionPotentiallySymmetric(unsigned OpCode) {
+  switch (OpCode) {
   case Instruction::FAdd:
   case Instruction::FSub:
   case Instruction::FMul:
@@ -992,14 +992,12 @@ ComplexDeinterleavingGraph::CompositeNode *
 ComplexDeinterleavingGraph::identifySymmetricOperation(ComplexValues &Vals) {
   auto *FirstReal = cast<Instruction>(Vals[0].Real);
   unsigned FirstOpc = FirstReal->getOpcode();
+  if (!isInstructionPotentiallySymmetric(FirstOpc))
+    return nullptr;
   for (auto &V : Vals) {
     auto *Real = cast<Instruction>(V.Real);
     auto *Imag = cast<Instruction>(V.Imag);
     if (Real->getOpcode() != FirstOpc || Imag->getOpcode() != FirstOpc)
-      return nullptr;
-
-    if (!isInstructionPotentiallySymmetric(Real) ||
-        !isInstructionPotentiallySymmetric(Imag))
       return nullptr;
 
     if (isa<FPMathOperator>(FirstReal))
@@ -1034,7 +1032,7 @@ ComplexDeinterleavingGraph::identifySymmetricOperation(ComplexValues &Vals) {
 
   auto Node =
       prepareCompositeNode(ComplexDeinterleavingOperation::Symmetric, Vals);
-  Node->Opcode = FirstReal->getOpcode();
+  Node->Opcode = FirstOpc;
   if (isa<FPMathOperator>(FirstReal))
     Node->Flags = FirstReal->getFastMathFlags();
 
